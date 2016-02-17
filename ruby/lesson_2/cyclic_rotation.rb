b030d5c3d9c2b6ddd11285a374f9a8b94d2fe7bb
Ariel Presa <arielpresa@gmail.com>
2016-02-17 01:58:22 +0000

    Last tweak for cyclic_rotation.rb to make it 100%

--- ruby/lesson_2/cyclic_rotation.rb
+++ ruby/lesson_2/cyclic_rotation.rb
@@ -1,14 +1,10 @@
 def solution(a, k)
-  unless a.empty? || a.nil?
-    unless k.nil? || k == 0
-      k = k - a.length if k >= a.length
-      first = a.slice(a.length-k, a.length)
-      last = a.slice(0, a.length-k)
-      (first.nil? ? [] : first) + (last.nil? ? [] : last)
-    else
-      a
-    end
+  unless a.empty? || k.nil? || k == 0
+    k = k % a.length if k >= a.length
+    first = a.slice(a.length-k, a.length)
+    last = a.slice(0, a.length-k)
+    (first.nil? ? [] : first) + (last.nil? ? [] : last)
   else
-    []
+    a
   end
 end
